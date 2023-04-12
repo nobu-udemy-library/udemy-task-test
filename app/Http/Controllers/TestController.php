a02c5223ab8124ea475beca075711c1d4f3fd39e
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-12 13:18:52 +0900

    ✨ eloquentのテスト

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,9 +9,14 @@ class TestController extends Controller
 {
   public function index()
   {
+    // Eloquent（エロクアント）
     $values = Test::all();
+    $count = Test::count();
+    $first = Test::findOrFail(1);
+    $whereBBB = Test::where("text", "=", "bbb")->get();
 
     // dd($values);
+
     return view("tests.test", compact("values"));
   }
 }
