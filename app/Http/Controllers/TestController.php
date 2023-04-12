79221a9c2a7a1fb7b174bfccf891e60f66780187
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-12 11:32:23 +0900

    ✨ DBの値をView側へ渡す

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
-    public function index() {
-      return view('tests.test');
-    }
+  public function index()
+  {
+    $values = Test::all();
+
+    // dd($values);
+    return view("tests.test", compact("values"));
+  }
 }
-
-
--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -1,1 +1,6 @@
-test
+test<br>
+
+@foreach ($values as $value)
+  {{ $value->id }}
+  {{ $value->text }}
+@endforeach
