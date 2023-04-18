480dbe9f7221ae6f8abc31c536b40990d37fea12
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-18 20:05:12 +0900

    ✨ ContactFormの新規登録のmvc作成

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -11,7 +11,7 @@
         <div class="p-6 text-gray-900">
           {{-- ContactForm --}}
           <section class="text-gray-600 body-font relative">
-            <form method="post" action="">
+            <form method="post" action="{{ route('contacts.store') }}">
               @csrf
               <div class="container px-5 mx-auto">
                 <div class="lg:w-1/2 md:w-2/3 mx-auto">
