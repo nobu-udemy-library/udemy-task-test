b3e176dc08ad091ae2e95451352bafad16d7e4c3
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-19 17:40:48 +0900

    ✨ updateのmvcの作成

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::prefix('contacts')
     Route::post('/', 'store')->name('store');
     Route::get('/{id}', 'show')->name('show');
     Route::get('/{id}/edit', 'edit')->name('edit');
+    Route::post('/{id}', 'update')->name('update');
   });
 
 Route::get('/', function () {
