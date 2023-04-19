ad40675e6b6fb8b3b28565b8d2dddeaf5994c4c5
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-19 14:46:44 +0900

    🩹 showのviewを変更

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -79,10 +79,13 @@
                       </p>
                     </div>
                   </div>
-                  <div class="p-2 w-full">
-                    <button
-                      class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
-                  </div>
+                  <form method="get" action="{{ Route('contacts.edit', ['id' => $contact->id]) }}" class="w-full">
+                    <div class="p-2 w-full">
+                      <button
+                        class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集画面へ
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
           </section>
