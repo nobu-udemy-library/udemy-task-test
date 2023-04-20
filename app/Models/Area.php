a556ac3727111fd0d243b26f748ae1bdebbd9766
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-20 13:56:55 +0900

    ✨ 1対多のリレーション

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  // * リレーション設定 1対多の関係 関数名は複数形
+  public function shops()
+  {
+    return $this->hasMany(Shop::class);
+  }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  // * リレーション設定 多対1の関係 関数名は単数
+  public function area()
+  {
+    return $this->belongsTo(Area::class);
+  }
 }
