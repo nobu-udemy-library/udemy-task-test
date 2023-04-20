a2c1f67ef022245408cf61b0723bf9f9637b98af
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-20 10:26:57 +0900

    ✨ Test用のseederの作成と登録実行

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -4,16 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class TestSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('tests')->insert([
+      [
+        'text' => 'aaa',
+      ],
+      [
+        'text' => 'bbb',
+      ],
+    ]);
+  }
 }
