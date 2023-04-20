929f5667ab6741c6010b4180b05a6bc597415c9a
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-20 09:16:29 +0900

    ♻️ showアクションのロジックをServiceに切りかける

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ContactForm;
+use App\Services\CheckFormService;
 
 class ContactFormController extends Controller
 {
@@ -64,31 +65,10 @@ class ContactFormController extends Controller
   {
     $contact = ContactForm::find($id);
 
-    // 年齢や性別などはDBへ数値で保存しているため、数値を日本語に変換してviewへ渡す
-    if ($contact->gender == 0) {
-      $gender = '男性';
-    } else {
-      $gender = '女性';
-    }
+    // Servicesへ性別確認の処理切り分け
+    $gender = CheckFormService::checkGender($contact);
 
-    if ($contact->age === 1) {
-      $age = '~19歳';
-    }
-    if ($contact->age === 2) {
-      $age = '20~29歳';
-    }
-    if ($contact->age === 3) {
-      $age = '30~39歳';
-    }
-    if ($contact->age === 4) {
-      $age = '40~49歳';
-    }
-    if ($contact->age === 5) {
-      $age = '50~59歳';
-    }
-    if ($contact->age === 6) {
-      $age = '60歳~';
-    }
+    $age = CheckFormService::checkAge($contact);
 
     return view('contacts.show', compact('contact', 'gender', 'age'));
   }
--- app/Services/CheckFormService.php
+++ app/Services/CheckFormService.php
@@ -3,11 +3,39 @@ namespace App\Services;
 
 class CheckFormService
 {
-  public static function checkGender()
+  public static function checkGender($contact)
   {
+    // 年齢や性別などはDBへ数値で保存しているため、数値を日本語に変換してviewへ渡す
+    if ($contact->gender == 0) {
+      $gender = '男性';
+    } else {
+      $gender = '女性';
+    }
+
+    return $gender;
   }
 
-  public static function checkAge()
+  public static function checkAge($contact)
   {
+    if ($contact->age === 1) {
+      $age = '~19歳';
+    }
+    if ($contact->age === 2) {
+      $age = '20~29歳';
+    }
+    if ($contact->age === 3) {
+      $age = '30~39歳';
+    }
+    if ($contact->age === 4) {
+      $age = '40~49歳';
+    }
+    if ($contact->age === 5) {
+      $age = '50~59歳';
+    }
+    if ($contact->age === 6) {
+      $age = '60歳~';
+    }
+
+    return $age;
   }
 }
